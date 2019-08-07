381d6117c64c88425b9f5cb543ab7d514b3f344b
mrmtshmp <mr.mt.shm@gmail.com>
2019-08-07 16:14:54 +0900

    First submit

---
+++ R/mf.sim.conditionalPoisson.R
@@ -0,0 +1,103 @@
+#'Simulate data from Poisson distribution and calculate coefficients
+#'
+#'@import dplyr
+#'@import plyr
+#'@import tibble
+#'
+#'
+#'@importFrom gnm gnm
+#'@importFrom bindata rmvbin
+#'
+#'@param vect.margprob_GS_1 <object; input>
+#'@param vect.margprob_GS_0 <object; input>
+#'@param mat.cov_GS_1  <object; input>
+#'@param mat.cov_GS_0  <object; input>
+#'@param n.sim  <numeric; proccessing>
+#'@param sim.N_1  <numeric; proccessing>
+#'@param sim.N_0  <numeric; proccessing>
+#'
+#'@export
+#'
+
+
+# mf for Simulation -------------------------------------------------------
+
+mf.sim.condPoisson <- function(
+  vect.margprob_GS_1,
+  vect.margprob_GS_0,
+  mat.cov_GS_1,
+  mat.cov_GS_0,
+  n.sim,
+  sim.N_1,
+  sim.N_0
+  ){
+  
+  .data <- data
+  
+  df.pmt <- data.frame(
+    i=seq(1:n.sim),
+    j=seq(1:n.sim)
+  )
+  
+  output <- ddply(
+    df.pmt,
+    .(i),
+    function(itt){
+      i.numb <- unique(itt$i)
+      D <- .data
+      df.sim_GS_1 <- rmvbin(
+        sim.N_1, 
+        margprob = vect.margprob_GS_1[c(2,3)], 
+        bincorr = mat.cov_GS_1
+      ) %>%
+        data.frame() %>%
+        mutate(GS=1)
+      
+      df.sim_GS_0 <- bindata::rmvbin(
+        sim.N_0, 
+        margprob = vect.margprob_GS_0[c(2,3)], 
+        bincorr = mat.cov_GS_0
+        ) %>%
+        data.frame() %>%
+        mutate(GS=0)
+      
+      df.sim <- df.sim_GS_0 %>%
+        rbind(df.sim_GS_1)
+      
+      colnames(df.sim) <- cov.names[c(2,3,1)]
+      
+      df.sim$ID <- seq(1:nrow(df.sim))
+      
+      df.sim.False <- df.sim %>%
+        mutate(
+          TotalFalse_Test_1 = abs(GS-Test_1),
+          TotalFalse_Test_2 = abs(GS-Test_2)
+        ) %>%
+        dplyr::select(
+          ID,
+          starts_with(
+            'TotalFalse'
+          )
+        ) %>%
+        gather(var, val, -ID)
+      print(
+        c(
+          sum(df.sim.False[df.sim.False$var=='TotalFalse_Test_1','val']), 
+          sum(df.sim.False[df.sim.False$var=='TotalFalse_Test_2','val'])
+        )
+      )
+      m1a <- gnm(
+        val ~ 1 + var, eliminate = as.factor(ID), 
+        data = df.sim.False, 
+        family = poisson(link = "log")
+        ) %>% 
+        summary() %>%
+        coef() %>%
+        data.frame()%>%
+        rownames_to_column('terms')
+      
+      return(m1a)
+    }
+  )
+  return(output)
+}
---
+++ R/mf.wrap.sim.Poisson.R
@@ -0,0 +1,109 @@
+#'Power analysis of conditional Poisson or mixed-effect Poisson
+#'
+#'@import dplyr
+#'@import plyr
+#'@import tibble
+#'
+#'@importFrom gnm gnm
+#'
+#'@param data <object; input> A data.frame-class object .
+#'@param dir.output <character string; output> Directory to output results.
+#'@param sim.N <numeric; processing> Itteration number.
+#'@param n.sim <numeric; processing> N of observation for simulation.
+#'@param cov.name <character vector; input>
+#'@param method <character; proccessing; preset="cPoisson"> cPoisson or glmerPoisson
+#'
+#'@export
+#'
+
+mf.wrap.sim.Poisson <- function(
+
+  data,
+  dir.sub     = "./sub",
+  dir.output = "../Output/190803",
+  sim.N      = 640,
+  n.sim      = 200,
+  cov.names  = c('GS','Test_1','Test_2'),
+  method     = "cPoisson",
+  prevalance = seq(0.3, 0.4, 0.02)
+  ){
+  
+  if(method=="cPoisson"){
+    formula.test <- mf.sim.condPoisson
+    }else formula.test <- mf.sim.glmerPoisson
+  
+  
+  
+  data_GS_1 <- data %>% filter(get(cov.names[1])==1)
+  data_GS_0 <- data %>% filter(get(cov.names[1])==0)
+  
+  data_GS_1[
+    nrow(data_GS_1) + 1,
+    cov.names
+    ] <- c(1,0.5,0.5)
+  
+  data_GS_0[
+    nrow(data_GS_0) + 1,
+    cov.names
+    ] <- c(1,0.5,0.5)
+  
+  
+  # calculation of cov matrix -----------------------------------------------
+  
+  mat.cov_GS_1 <- 
+    data_GS_1[,cov.names[c(2,3)]] %>%
+    cor()
+  
+  mat.cov_GS_0 <- 
+    data_GS_0[,cov.names[c(2,3)]] %>%
+    cor()
+  
+  vect.margprob_GS_1 <- 
+    c(
+      sum(data_GS_1[,cov.names[1]]),
+      sum(data_GS_1[,cov.names[2]]), 
+      sum(data_GS_1[,cov.names[3]])
+      )/ (nrow(data_GS_1))
+  
+  vect.margprob_GS_0 <- 
+    c(
+      sum(data_GS_0[,cov.names[1]]),
+      sum(data_GS_0[,cov.names[2]]), 
+      sum(data_GS_0[,cov.names[3]])
+      )/ (nrow(data_GS_0))
+  
+  for(i in 1:length(prevalance)){
+    
+    prev <- prevalance[i]
+    
+    test <-  formula.test(
+      vect.margprob_GS_1,
+      vect.margprob_GS_0,
+      mat.cov_GS_1,
+      mat.cov_GS_0,
+      n.sim,
+      sim.N_1 = floor(sim.N*prev),
+      sim.N_0 = floor(sim.N*(1-prev))
+      )
+    
+    p.val_beta1 <-
+      test %>% filter(terms=='varTotalFalse_Test_2')
+    
+    plot(
+      ecdf(p.val_beta1[,'Pr...z..']), 
+      main=sprintf(
+        'GS:1, n=%s; GS:0, n=%s', 
+        floor(sim.N*prev), 
+        floor(sim.N*(1-prev))
+      )
+    )
+    abline(
+      h=0.8, 
+      v=c(0.10,0.05,0.025)
+      )
+    }
+  }
+
+# Endrant -----------------------------------------------------------------
+
+
